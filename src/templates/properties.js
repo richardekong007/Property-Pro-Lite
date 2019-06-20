3ee3971943ef27b1f606f7a947e5662f9dbc3224
Richard <ekong.richard@yahoo.com>
2019-06-20 08:48:26 +0100

    feature(Properties Page) Add features to view properties by type modify properties page template in src/templates/properties.js by adding select UI components modify style.css by add style rules for the select UI component execute build scripts to translate ES6.
    [start #166689211]

--- src/templates/properties.js
+++ src/templates/properties.js
@@ -2,8 +2,21 @@ exports.render = () => {
     
     return `
         <div class = "property-container">
-            <div id = "header-text">Property Adverts</div>
-            <div></div>
+            <div id = "properties-title">Property Adverts</div>
+            <div class = "property-type-holder">
+            <form>
+                    <label>Search by:</label>
+                        <select class = "property-type-options">
+                            <option value = "Property type"> Property type</option>
+                            <option value = "Self-contained">Self-contained</option>
+                            <option value = "2 Bedroom">2 Bedroom</option>
+                            <option value = "3 Bedroom">3 Bedroom</option>
+                            <option value = "Mini flat">Mini flat</option>
+                            <option value = "Duplex">Duplex</option>
+                            <option value = "Bungalow">Bungalow</option>
+                        </select>
+                </form>
+            </div>
             <div id = "properties-grid"></div>
             <div id = "add-property-button" class = "fab tooltip">+
                 <span class = "tooltiptext small-text">Add property</span>
