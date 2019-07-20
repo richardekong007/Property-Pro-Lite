5bcb8ae13a516157bbf6c52d1328a2be2f27d75f
Richard <ekong.richard@yahoo.com>
2019-07-20 20:25:16 +0100

    ft(consume endpoint) consume signup endpoint
    [finishes #167414254]

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,5 +1,6 @@
 import {render} from "../templates/signup";
 import TinyEmitter from "tiny-emitter";
+import config from "../config.js";
 
 class Signup extends TinyEmitter{
     constructor(container){
@@ -16,21 +17,55 @@ class Signup extends TinyEmitter{
 
     addEventListener(){
         this.openSigninPage();
+        this.signupClick();
     }
 
     openSigninPage(){
-        const form = this.container.querySelector("#signup-form");
         const signinText = this.container.querySelector("#signin-text");
-        form.addEventListener("submit", event => {
-            event.preventDefault();
-            if (event.target.querySelector("button")){
-                this.emit("signin");
-            }
-        });
         signinText.addEventListener("click", event =>{
             event.preventDefault();
             this.emit("signin_click");
         });
     }
+
+    signupClick (){
+        const form = this.container.querySelector("#signup-form");
+        form.addEventListener("submit", event =>{
+            event.preventDefault();
+            const firstName = event.target.querySelector("[data-first-name]");
+            const lastName = event.target.querySelector("[data-last-name]");
+            const email = event.target.querySelector("[data-email]");
+            const password = event.target.querySelector("[data-password]");
+            const phone = event.target.querySelector("[data-phone]");
+            const address = event.target.querySelector("[data-address]");
+            this.signupUser({
+                email:email.value,
+                first_name: firstName.value,
+                last_name: lastName.value,
+                password:password.value,
+                address:address.value,
+                phone_number:phone.value
+            });
+        });
+    }
+
+    signupUser (data){
+        fetch(`${config.baseUrl}/api/v1/auth/signup`,{
+            mode:"cors",
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(res => {
+            if (res.error){
+                return Promise.reject(res.error);
+            }
+            this.emit("signup", res.data);
+        })
+        .catch(err => this.emit("error", err));
+    }
+
+    
 }
 export default Signup;
