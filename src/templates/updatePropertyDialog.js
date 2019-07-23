ae7d69b136ab58db6a4254dd83d3fbd945ec3203
Richard <ekong.richard@yahoo.com>
2019-07-23 12:55:59 +0100

    ft(consume endpoint) implement feature to update property price modify src/templates/updatePropertyDialog.js by adding method to update property and emiting update event modify app.js by observing update events
    [finishes #167453992]

--- src/templates/updatePropertyDialog.js
+++ src/templates/updatePropertyDialog.js
@@ -5,22 +5,7 @@ const template = `
             <button class = "close-rect smaller-text">x</button>
         </div>
         <form class = "property-form">
-            <input type = "text" placeholder = "Property Address" title = "Property Address" required/>
-            <input type = "text" placeholder = "Property City" title = "Property City" required/>
-            <br>
-            <input type = "text" placeholder = "Property State" title = "Property State" required/>
-            <select class = "property-type">
-                <option value = "Property type"> Property type</option>
-                <option value = "Self-contained">Self-contained</option>
-                <option value = "2 Bedroom">2 Bedroom</option>
-                <option value = "3 Bedroom">3 Bedroom</option>
-                <option value = "Mini flat">Mini flat</option>
-                <option value = "Duplex">Duplex</option>
-                <option value = "Bungalow">Bungalow</option>
-            </select>
-            <br>
-            <input type = "number" placeholder = "Property Price" title = "Property Price" required/>
-            <input type = "file"/>
+            <input type = "number" placeholder = "Property Price" title = "Property Price" data-price required/>
             <br>
             <label class = "checkbox bit-smaller-text">
                 <input type = "checkbox" />
