223a04182a9aa8660ce362a26ebd471775b0dfdd
Richard <ekong.richard@yahoo.com>
2019-07-24 16:55:26 +0100

    ch(replace alert) create custom dialog to convey error and information messages to user add vector graphics for error and information dialogs create templates and component classes for both ErrorDialog and InformationDialog display the custom dialogs based on an event in app.js add new style rules in style.css to style the dialog templates accordingly
    [finishes #167477692]

---
+++ src/templates/informationDialog.js
@@ -0,0 +1,19 @@
+
+const template = `
+<div class = "dialog-container">
+    <div class = "dialog-header">
+        <span class = "dialog-title">Information</span>
+    </div>
+    <div class = "content bit-smaller-text">
+        <div class = "message-holder">
+            <img src = "./vectors/info.svg" alt = "info" width = "25px" height = "25px"/>
+            <span class = "message"/> 
+        </div>
+        <div class = "button-holder">
+            <button>Ok</button>
+        </div>
+    </div>
+</div>
+`;
+
+export default template;
